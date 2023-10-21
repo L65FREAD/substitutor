387d7de7e2072ed8cc3372111a5608f1ebd1ac9b
Jtawfik04 <johntawfik106@gmail.com>
2023-10-21 13:49:19 -0400

    finished pop-up

--- popup.js
+++ popup.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Your popup JavaScript code goes here
+document.addEventListener('DOMContentLoaded', function () {
+  const languageSelect = document.getElementById('languageSelect');
+  const difficultyLabel = document.getElementById('difficultyLabel');
+  const difficultyRange = document.getElementById('difficultyRange');
+
+  languageSelect.addEventListener('change', function (e) {
+    const selectedLanguage = e.target.value;
+    // Handle the language change
+  });
+
+  difficultyRange.addEventListener('input', function (e) {
+    const value = e.target.value;
+    let difficulty = 'Medium';
+    if (value === '1') {
+      difficulty = 'Easy';
+    } else if (value === '2') {
+      difficulty = 'Medium';
+    } else if (value === '3') {
+      difficulty = 'Hard';
+    }
+    difficultyLabel.textContent = `Difficulty: ${difficulty}`;
+  });
 });
